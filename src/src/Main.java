9baf9e9f1187995cdb27b55d43b50d965aa8e704
Tossmann <trautmann.phillip@gmx.de>
2016-10-14 12:43:17 +0200

    BigInteger Integration, Comparison of arguments

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,6 +3,29 @@ import java.math.BigInteger;
 
 public class Main {	
 	public static void main(String [] args){
-		
+
+		if(args.length > 1){
+
+
+			BigInteger a = new BigInteger(args[0]);
+			BigInteger b = new BigInteger(args[1]);
+
+			if(a.compareTo(b) == 1) 
+			{
+				System.out.println(a + " ist größer als " + b);
+			}
+			if(a.compareTo(b) == -1)
+			{
+				System.out.println(b + " ist größer als " + a);
+			}
+			if(a.compareTo(b) == 0)
+			{
+				System.out.println("Die eingegebenen Zahlen sind identisch. Der ggT lautet "+ args[0]);
+			}
+		}
+		else
+		{
+			System.out.println("Bitte starten Sie das Programm neu und übergeben 2 Argumente");
+		}
 	}
 }
